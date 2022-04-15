eeaec0458e477896b37d7f8980f9874890741cf0
MFromenteau <marvin.fromenteau@gmail.com>
2022-04-15 15:04:55 +0200

    wip css

--- src/app/stream-event/stream-displayer/stream-displayer.component.ts
+++ src/app/stream-event/stream-displayer/stream-displayer.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from 'src/app/core/services/account/account.service';
 import { IStreamEvent } from 'src/core/classes/istream-event';
 
 @Component({
@@ -9,7 +10,7 @@ import { IStreamEvent } from 'src/core/classes/istream-event';
 export class StreamDisplayerComponent implements OnInit {
   @Input() stream: IStreamEvent;
   @Output() removeRequest: EventEmitter<number> = new EventEmitter();
-  constructor() { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
   }
